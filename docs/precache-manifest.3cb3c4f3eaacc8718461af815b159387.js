6ca628e2e627aabd413041374cea849397f938b4
Adrian <adrgon@gmail.com>
2020-07-28 15:28:40 -0300

    Modificacion rutas index.html

---
+++ docs/precache-manifest.3cb3c4f3eaacc8718461af815b159387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5f9de0d44d876d04fc68739e3839ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d230d0f8a0a37ae071",
+    "url": "/static/css/main.d227f7a4.chunk.css"
+  },
+  {
+    "revision": "f9234fcf84c080de371a",
+    "url": "/static/js/2.982de7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.982de7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d230d0f8a0a37ae071",
+    "url": "/static/js/main.225b727b.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
